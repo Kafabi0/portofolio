55fb5077936e3706f767a1e7ece1aa8ed2a768cc
Kafabi Aulia Fasyah <150753433+Kafabi0@users.noreply.github.com>
2025-06-27 09:25:50 +0700

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,19 +6,19 @@ import ProfileCard from '../ProfileCard'
 export default function Profile() {
   return (
     <div className="p-10">
+            <h1 className="text-3xl font-bold">Halaman Profil</h1>
+      <p>Selamat datang di halaman profil.</p>
 <ProfileCard
-  name="Javi A. Torres"
+  name="Kafabi Aulia Fasyah"
   title="Software Engineer"
-  handle="javicodes"
-  status="Online"
+  handle="z312xy"
   contactText="Contact Me"
-  avatarUrl="/path/to/avatar.jpg"
+  avatarUrl="../assets/foto1.jpeg"
   showUserInfo={true}
   enableTilt={true}
   onContactClick={() => console.log('Contact clicked')}
 />
-      <h1 className="text-3xl font-bold">Halaman Profil</h1>
-      <p>Selamat datang di halaman profil.</p>
+
     </div>
   );
 }
