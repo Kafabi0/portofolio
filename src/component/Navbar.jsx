5e4c3f9510ba6bc41993c9c50dfc2610c77d6092
Kafabi Aulia Fasyah <150753433+Kafabi0@users.noreply.github.com>
2025-06-28 09:12:04 +0700

    lagui

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -27,9 +27,6 @@ export default function Navbar() {
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-white/80 backdrop-blur-md shadow-md z-50">
-      <div className="absolute top-4 right-4 z-50">
-        <Lanyard />
-      </div>
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
         <h1 className="text-xl font-bold text-blue-500">SiKaFa</h1>
 
