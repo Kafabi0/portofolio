dc4d64442bc0bd7216d2385b0f13667cefa0d628
Kafabi Aulia Fasyah <150753433+Kafabi0@users.noreply.github.com>
2025-06-28 10:31:57 +0700

    z-

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white/80 backdrop-blur-md shadow-md z-50">
+    <nav className="fixed top-0 left-0 w-full bg-white/80 backdrop-blur-md shadow-md z-100">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
         <h1 className="text-xl font-bold text-blue-500">SiKaFa</h1>
 
